5015ba139e00d21f2e6201dbc97709529fa8fef5
Jesus <jesustrujillo5023@gmail.com>
2019-05-06 10:28:19 +0200

    Starting project with configuration

---
+++ scrum-methodology-app/src/main/java/es/uma/scrum/ScrumMethodologyAppApplication.java
@@ -0,0 +1,13 @@
+package es.uma.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumMethodologyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumMethodologyAppApplication.class, args);
+	}
+
+}
